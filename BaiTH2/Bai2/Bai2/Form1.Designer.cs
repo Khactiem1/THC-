ff4528222dbef9e13a3640f9603b3b65fe4051af
Khactiem1 <nguyenkhactiem2k1@gmail.com>
2021-05-02 20:54:37 +0700

    th2

---
+++ BaiTH2/Bai2/Bai2/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Bai2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(43, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(422, 501);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Listbox";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(494, 40);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(422, 501);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Xử lý listbox";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(410, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(410, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Nhập";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(6, 160);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(410, 329);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(410, 45);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Tổng các phần tử trong list";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(410, 48);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Xoá phần tử đầu và cuối";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 157);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(410, 46);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Xoá phần tử đang chọn";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 226);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(410, 46);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Tăng mỗi phần tử lên 2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 288);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(410, 48);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Thay bằng bình phương";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(6, 356);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(410, 45);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "Chọn số chẵn";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 421);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(410, 41);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "Chọn số lẻ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(49, 564);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(861, 49);
+            this.button9.TabIndex = 2;
+            this.button9.Text = "Kết thúc";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 652);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ BaiTH2/Bai2/Bai2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            while(listBox1.SelectedIndices.Count > 0)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndices[0]);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            listBox1.Items.Add(x);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(0);
+            listBox1.Items.RemoveAt(listBox1.Items.Count - 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                int x = (int)listBox1.Items[i];
+                sum += x;
+            }
+            MessageBox.Show("Tổng là: " + sum);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                int x = (int)listBox1.Items[i] + 2;
+                listBox1.Items[i] = x;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                int x = (int)listBox1.Items[i] * (int)listBox1.Items[i];
+                listBox1.Items[i] = x;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = -1;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if((int)listBox1.Items[i] % 2 == 0)
+                {
+                    listBox1.SelectedIndex = i;
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = -1;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if ((int)listBox1.Items[i] % 2 != 0)
+                {
+                    listBox1.SelectedIndex = i;
+                }
+            }
+        }
+    }
+}
